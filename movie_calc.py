3e6adcbb3d35f2b8c5b216cd1c2bcb441280c492
AJ <121971319+viraux@users.noreply.github.com>
2023-04-18 10:57:36 -0400

    Add files via upload

--- movie_calc.py
+++ movie_calc.py
@@ -3,6 +3,7 @@ import sqlite3
 import json
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 import re
 import requests
 
@@ -41,7 +42,27 @@ def runtimeByGenre(cur):
 
     return d
         
+def percentDif(data):
+    d = {}
+    items = list(data.items())
+    for i in range(len(items)):
+        pair = items[i]
+        # print(pair[0])
+        if i == 0:
+            d[pair[0]] = 1
+            pass
+        # print(items[i])
+        else:
+            prev_pair = items[0]
+            # prev_pair = items[i-1]
+            dif = pair[1] - prev_pair[1]
+            percentage = dif / (prev_pair[1])
+            # print(percentage)
+            d[pair[0]] = round(percentage,2) + 1
 
+    # print(d)
+    return d
+    
     pass
 
 #find the average score by rating
@@ -66,15 +87,54 @@ def scoreByRating(cur):
 def plotYear(data):
     fig = plt.figure(figsize=(10,5))
     ax = fig.add_subplot(111)
+    x_list = []
+    y_list = []
     for year in data:
         x = year
         y = data[year]
+        x_list.append(x)
+        y_list.append(y)
         ax.scatter(x, y, color='k', linewidth=2)
+
+    x_list = np.array(x_list)
+    y_list = np.array(y_list)
+    a, b = np.polyfit(x_list, y_list, 1)
+    plt.plot(x_list, a*x_list+b, color='red')
 
     ax.set_xlabel("Year")
     ax.set_ylabel("Average Runtime")
 
+
     fig.savefig("Average_Runtime_by_Year")
+
+    plt.show()
+        
+        
+    pass
+
+# plot year by percentage difference
+def plotPercent(data):
+    fig = plt.figure(figsize=(10,5))
+    ax = fig.add_subplot(111)
+    x_list = []
+    y_list = []
+    for year in data:
+        x = year
+        y = data[year]
+        x_list.append(x)
+        y_list.append(y)
+        ax.scatter(x, y, color='k', linewidth=2)
+
+    x_list = np.array(x_list)
+    y_list = np.array(y_list)
+    a, b = np.polyfit(x_list, y_list, 1)
+    plt.plot(x_list, a*x_list+b, color='blue')
+
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Percent Change in Average Runtime")
+
+
+    fig.savefig("Percent_Average_Runtime_by_Year")
 
     plt.show()
         
@@ -116,6 +176,36 @@ def plotRating(data):
     pass
 
 
+def writeData(data_l):
+    final_d = {}
+    ratings = ['PG','PG-13','G','R','Not Rated']
+    final_d["year_data"] = {}
+    for d in data_l:
+        keys = list(d.keys())
+        if type(keys[0]) == int:
+            if d[keys[0]] == 1:
+                final_d["year_data"]["percent_change"] = d
+                pass
+            else:
+                final_d["year_data"]["avg_runtime"] = d
+                pass
+        else:
+            if keys[0] in ratings:
+                final_d["rating_score"] = d
+                pass
+            else:
+                final_d["genre_runtime"] = d
+                pass
+
+    # print(final_d)
+        
+    with open("movies.json","w") as f:
+        json.dump(final_d,f, indent=2)
+
+
+    pass
+
+
 
 
 
@@ -123,18 +213,31 @@ def plotRating(data):
 
 
 def main():
+    data_l = []
     cur = setUpDatabase("movies.db")
     year_d = runtimeByYear(cur)
-    print(year_d)
-    plotYear(year_d)
+    # print(year_d)
+    data_l.append(year_d)
+    percent_d = percentDif(year_d)
+    data_l.append(percent_d)
+    # plotPercent(percent_d)
+    # plotYear(year_d)
 
     genre_d = runtimeByGenre(cur)
-    print(genre_d)
-    plotGenre(genre_d)
+    # print(genre_d)
+    data_l.append(genre_d)
+    # plotGenre(genre_d)
 
     score_d = scoreByRating(cur)
-    print(score_d)
-    plotRating(score_d)
+    # print(score_d)
+    data_l.append(score_d)
+    # plotRating(score_d)
+
+    # print(data_l)
+
+    writeData(data_l)
+
+    
 
 
     pass
@@ -147,3 +250,9 @@ main()
 # List old resources we used such a HW's?  Don't need to
 # Should all our calculations be written to one file?  Can be multiple
 #Should one of the visualizations have data from both APIS? Try to have them include both
+
+# Presentation Notes:
+# Completely seperate data/visualizations, two seperate data files in presentation one
+# put number of barchart on graph?
+# make sure only 25 each time (probably just avoid driver file)
+
